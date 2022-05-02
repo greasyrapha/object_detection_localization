99535c82a259cc37cf8b5412355a8254d83f03b3
GreasyRapha <99736012+greasyrapha@users.noreply.github.com>
2022-05-02 20:08:42 +0900

    add depth code

--- detection/scripts/detection.py
+++ detection/scripts/detection.py
@@ -17,100 +17,113 @@ from utils.plots import Annotator, colors
 from utils.torch_utils import select_device, time_sync
 from utils.augmentations import letterbox
 from models.experimental import attempt_load
+from cv_bridge import CvBridge, CvBridgeError
+import pyrealsense2 as rs2
 from pathlib import Path
 sys.path.remove('/opt/ros/melodic/lib/python2.7/dist-packages')
 
+class Detector:
+    def __init__(self, color_image_topic, depth_image_topic):
+        self.bridge = CvBridge()
+        self.subcolor = rospy.Subscriber(color_image_topic, Image, self.imageColorCallback, queue_size=1, buff_size=52428800)
+        self.subdepth = rospy.Subscriber(depth_image_topic, Image, self.imageDepthCallback, queue_size=1, buff_size=52428800)
 
-def LoadImg(img):
-    img_size = 640
-    cap = None
-    path = None
-    img0 = img
-    img = letterbox(img0, new_shape=img_size)[0]
-    img = img[:, :, ::-1].transpose(2, 0, 1)
-    img = np.ascontiguousarray(img)
-    return path, img, img0, cap
+    def imageColorCallback(self, colordata):
+        callback_colorimage = self.bridge.imgmsg_to_cv2(colordata, colordata.encoding)
+        callback_colorimage = callback_colorimage[...,::-1]
+        with torch.no_grad():
+            self.detection(callback_colorimage)
 
-def run(image):
-    dataset = LoadImg(image)
+    def imageDepthCallback(self, depthdata):
+        self.callback_depthimage = self.bridge.imgmsg_to_cv2(depthdata, depthdata.encoding)
 
-    path = dataset[0]
-    img = dataset[1]
-    im0s = dataset[2]
-    vid_cap = dataset[3]
+    def Loadimage(self, img):
+        cap = None
+        path = None
+        img0 = img
+        img = letterbox(img0, new_shape=640)[0]
+        img = img[:, :, ::-1].transpose(2, 0, 1)
+        img = np.ascontiguousarray(img)
+        return path, img, img0, cap
 
-    w = str(weights)
-    stride, names = 64, [f'class{i}' for i in range(1000)]  # assign defaults
-    names = model.module.names if hasattr(model, 'module') else model.names
-    model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))
+    def detection(self, image):
+        dataset = self.Loadimage(image)
 
-    dt, seen = [0.0, 0.0, 0.0], 0
+        path = dataset[0]
+        img = dataset[1]
+        im0s = dataset[2]
+        vid_cap = dataset[3]
+    
+        w = str(weights)
+        stride, names = 64, [f'class{i}' for i in range(1000)]  # assign defaults
+        names = model.module.names if hasattr(model, 'module') else model.names
+        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))
 
-    t1 = time_sync()
+        dt, seen = [0.0, 0.0, 0.0], 0
 
-    img = torch.from_numpy(img).to(device)
-    img = img.float()  # uint8 to fp16/32
-    img = img / 255.0  # 0 - 255 to 0.0 - 1.0
-    if len(img.shape) == 3:
-        img = img[None]  # expand for batch dim
+        t1 = time_sync()
 
-    t2 = time_sync()
-    dt[0] += t2 - t1
+        img = torch.from_numpy(img).to(device)
+        img = img.float() 
+        img = img / 255.0
+        if len(img.shape) == 3:
+            img = img[None]
 
-    pred = model(img, augment=False, visualize=False)[0]
+        t2 = time_sync()
+        dt[0] += t2 - t1
 
-    t3 = time_sync()
-    dt[1] += t3 - t2
+        pred = model(img, augment=False, visualize=False)[0]
 
-    pred = non_max_suppression(pred, 0.3, 0.45, None, 1000)
+        t3 = time_sync()
+        dt[1] += t3 - t2
 
-    dt[2] += time_sync() - t3
+        pred = non_max_suppression(pred, 0.3, 0.45, None, 1000) # conf_thres, iou_thres, ?, max_def
 
-    for i, det in enumerate(pred):
-        seen += 1
-        p, s, im0, frame = path, '', im0s.copy(), getattr(dataset, 'frame', 0)
-        s += '%gx%g ' % img.shape[2:]  # print string
-        gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
-        imc = im0
-        annotator = Annotator(im0, line_width=3, example=str(names))
-        if len(det):
-            # Rescale boxes from img_size to im0 size
-            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+        dt[2] += time_sync() - t3
 
-            # Print results
-            for c in det[:, -1].unique():
-                n = (det[:, -1] == c).sum()  # detections per class
-                s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+        for i, det in enumerate(pred):
+            seen += 1
+            p, s, im0, frame = path, '', im0s.copy(), getattr(dataset, 'frame', 0)
+            #s += '%gx%g ' % img.shape[2:]  
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  
+            imc = im0
+            annotator = Annotator(im0, line_width=3, example=str(names))
+            if len(det):
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
 
-            # Write results
-            for *xyxy, conf, cls in reversed(det): # Add bbox to image
-                c = int(cls)  # integer class
-                label = f'{names[c]} {conf:.2f}'
-                annotator.box_label(xyxy, label, color=colors(c, True))
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "
 
-        # Print time (inference-only)
-        print(f'{s}Done. ({t3 - t2:.3f}s)')
+                for *xyxy, conf, cls in reversed(det): 
+                    c = int(cls)  
+                    xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()
+                    posx = int(xywh[0] * 640)
+                    posy = int(xywh[1] * 480)
+                    self.distance_cm = 0.1*self.callback_depthimage[posx, posy]
+                    label = f'{names[c]} {conf:.2f} {self.distance_cm:.2f}cm'
+                    annotator.box_label(xyxy, label, color=colors(c, True))
+                    print(f'{s}{self.distance_cm:.2f}cm')
 
-    cv2.imshow('Detection', im0)
-    cv2.waitKey(1)
+                #print(f'{s}Done. {1/(t3 - t2):.3f}fps')
 
-def image_callback(image):
-    callback_image = np.frombuffer(image.data, dtype=np.uint8).reshape(image.height, image.width, -1)
-    callback_image = callback_image[...,::-1] #BGR to RGB
-    with torch.no_grad():
-        run(callback_image)
+        cv2.imshow('Detection', im0)
+        cv2.waitKey(1)
+
+def main():
+    color_image_topic = '/camera/color/image_raw'
+    depth_image_topic = '/camera/depth/image_rect_raw'
+
+    detector = Detector(color_image_topic, depth_image_topic)
+    rospy.spin()
 
 if __name__ == "__main__": 
-    # Initialize
     set_logging()
     device = ''
     device = select_device(device)
     weights = '/home/jetson/catkin_ws/src/detection/scripts/best.pt'
     imgsz = 640
-
     model = attempt_load(weights, map_location=device)
 
     rospy.init_node('detection')
-    rospy.Subscriber("/camera/color/image_raw", Image, image_callback, queue_size=1, buff_size=52428800)
-
-    rospy.spin()
+    main()
